311018ee86e860ebec6dd73bfa72afdc7af42351
Reuben Ayres <ra@sent.com>
2018-11-14 17:05:46 -0600

    index route + template

--- controllers/ItemController.rb
+++ controllers/ItemController.rb
@@ -1,6 +1,11 @@
 # all other controllers inherit from ApplicationController
 class ItemController < ApplicationController
-  get '/test' do
-    "you hit item controller"
+  # index
+  get '/' do
+    # get all the items
+    # pass them to template
+    @items = Item.all # this would be like Item.find({}) in mongoose
+
+    erb :item_index
   end
 end
